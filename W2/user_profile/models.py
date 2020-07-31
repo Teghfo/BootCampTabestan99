c66948e7270ddeffd37cd44afa171ad247a7744d
Teghfo <teghfo@gmail.com>
2020-07-31 15:38:00 +0430

    django model field validator

--- W2/user_profile/models.py
+++ W2/user_profile/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete= models.CASCADE)
-    phone_number = models.CharField(max_length=50)
+    phone_regx = r'9\d{9}'
+    phone_regx = RegexValidator(phone_regx, message='''shomare mobile ro 
+                                                    dar format khaste shodeh vared nakardi: format dorost9xxxxxxxxx''')
+    phone_number = models.CharField(validators=[phone_regx], max_length=50)
     address = models.CharField(max_length=500, default='')
 
     class Meta:
