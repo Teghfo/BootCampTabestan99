df20b82cbe651d4469d9c6c68a92d7ea332ee552
Teghfo <teghfo@gmail.com>
2020-07-31 18:12:10 +0430

    django orm manager and save override

--- W2/user_profile/models.py
+++ W2/user_profile/models.py
@@ -2,14 +2,46 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 
+import re
+# from user_profile.models import Profile
+
+
+class ProfileManager(models.Manager):
+    def my_query(self):
+        from django.db import connection
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * from user_profile")
+            res_list = []
+            for i in cursor.fetchall():
+                res_list.append(i)
+        return res_list
+
 
 class Profile(models.Model):
+    gender_choices = [
+        ('M', 'Male'), 
+        ('F', 'Female')
+    ]
+
     user = models.OneToOneField(User, on_delete= models.CASCADE)
-    phone_regx = r'9\d{9}'
-    phone_regx = RegexValidator(phone_regx, message='''shomare mobile ro 
+    phone_regx = r'^9\d{9}$'
+    phone_regx = RegexValidator(regex=phone_regx, message='''shomare mobile ro 
                                                     dar format khaste shodeh vared nakardi: format dorost9xxxxxxxxx''')
     phone_number = models.CharField(validators=[phone_regx], max_length=50)
     address = models.CharField(max_length=500, default='')
+    admin = models.CharField(max_length=20, blank=True, null= True)
+    gender = models.CharField(max_length=1, null = True,choices=gender_choices)
+    objects = ProfileManager()
 
     class Meta:
         db_table = 'user_profile'
+
+    def save(self, *args, **kwargs):
+        phone_regx = r'^9\d{9}$'
+        if not re.match(phone_regx, self.phone_number):
+            raise '''khak tu saret'''
+        
+        super(Profile, self).save(*args, **kwargs)
+
+
+# User.objects.bulk_create
